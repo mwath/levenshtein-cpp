a94bc8fbfcbb87cddcc63e169388ad474f6308be
Mathieu Walravens <m.walravens@students.ephec.be>
2021-12-06 16:24:34 +0100

    Use memset

--- levenshtein/src/levenshtein.cpp
+++ levenshtein/src/levenshtein.cpp
@@ -1,6 +1,7 @@
 #include "levenshtein.hpp"
 #include <algorithm>
 #include <cstdint>
+#include <cstring>
 
 namespace lev {
 int distance(std::string a, std::string b) {
@@ -13,7 +14,8 @@ int distance(std::string a, std::string b) {
     if (cols == 0)
         return rows;
 
-    uint16_t matrix[rows][cols] = {};
+    uint16_t matrix[rows][cols];
+    std::memset(matrix, 0, rows * cols * sizeof(uint16_t));
 
     for (auto i = 0; i < rows; ++i)
         matrix[i][0] = i;
