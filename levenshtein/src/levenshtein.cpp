7878adcfa75e2c5ba4dc546b9ef6be030fc96771
Mathieu Walravens <m.walravens@students.ephec.be>
2021-12-06 16:50:47 +0100

    Use 1d array

--- levenshtein/src/levenshtein.cpp
+++ levenshtein/src/levenshtein.cpp
@@ -14,25 +14,25 @@ int distance(std::string a, std::string b) {
     if (cols == 0)
         return rows;
 
-    uint16_t matrix[rows][cols];
+    uint16_t* matrix = new uint16_t[rows * cols];
     std::memset(matrix, 0, rows * cols * sizeof(uint16_t));
 
     for (auto i = 0; i < rows; ++i)
-        matrix[i][0] = i;
+        matrix[i * cols] = i;
 
     for (auto i = 0; i < cols; ++i)
-        matrix[0][i] = i;
+        matrix[i] = i;
 
     for (auto j = 1; j < cols; ++j) {
         for (auto i = 1; i < rows; ++i) {
-            matrix[i][j] = std::min({
-                matrix[i - 1][j] + 1, // deletion
-                matrix[i][j - 1] + 1, // insertion
-                matrix[i - 1][j - 1] + (a[i - 1] != b[j - 1]) // substitution
+            matrix[i * cols + j] = std::min({
+                matrix[(i - 1) * cols * j] + 1, // deletion
+                matrix[i * cols + j - 1] + 1, // insertion
+                matrix[(i - 1) * cols + j - 1] + (a[i - 1] != b[j - 1]) // substitution
             });
         }
     }
 
-    return matrix[rows - 1][cols - 1];
+    return matrix[rows * cols - 1];
 }
 } // namespace lev
