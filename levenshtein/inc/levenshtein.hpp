563852e9a9e741c8cada698fd145121571d50fd0
Mathieu Walravens <m.walravens@students.ephec.be>
2021-12-07 09:57:07 +0100

    Bump version to 0.0.2

--- levenshtein/inc/levenshtein.hpp
+++ levenshtein/inc/levenshtein.hpp
@@ -3,7 +3,7 @@
 namespace lev {
 static constexpr unsigned VERSION_MAJOR = 0;
 static constexpr unsigned VERSION_MINOR = 0;
-static constexpr unsigned VERSION_PATCH = 1;
+static constexpr unsigned VERSION_PATCH = 2;
 
 int distance(std::string a, std::string b);
 } // namespace lev
